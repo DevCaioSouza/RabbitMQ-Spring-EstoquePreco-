9c0bbc1c8f251387aa8a62066ac7c4a7ee695a42
Caio Camargo <caiocbahia@hotmail.com>
2024-09-09 07:58:58 -0300

    Creating queues and add them on RabbitMQ

---
+++ src/main/java/com/microsservico/estoquepreco/connections/RabbitMQConnection.java
@@ -0,0 +1,53 @@
+package com.microsservico.estoquepreco.connections;
+
+import com.microsservico.estoquepreco.constants.RabbitMQConstants;
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConnection {
+
+    private static final String EXCHANGE_NAME = "amq.direct";
+    private AmqpAdmin amqpAdmin;
+
+    public RabbitMQConnection(AmqpAdmin amqpAdmin){
+        this.amqpAdmin = amqpAdmin;
+    }
+
+    private Queue queue(String queueName){
+        return new Queue(queueName, true, false, false);
+    }
+
+    private DirectExchange directExchange(){
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    private Binding relationship(Queue queue, DirectExchange directExchange){
+        return new Binding(queue.getName(), Binding.DestinationType.QUEUE, directExchange.getName(), queue.getName(), null);
+    }
+
+    @PostConstruct
+    private void add(){
+        Queue stockQueue = this.queue(RabbitMQConstants.STOCK_QUEUE);
+        Queue priceQueue = this.queue(RabbitMQConstants.PRICE_QUEUE);
+
+        DirectExchange exchange = this.directExchange();
+
+        Binding stockBinding = this.relationship(stockQueue, exchange);
+        Binding priceBinding = this.relationship(priceQueue, exchange);
+
+        //criando as filas no RabbitMQ
+        this.amqpAdmin.declareQueue(stockQueue);
+        this.amqpAdmin.declareQueue(priceQueue);
+
+        //criando as exchanges
+        this.amqpAdmin.declareExchange(exchange);
+
+        this.amqpAdmin.declareBinding(stockBinding);
+        this.amqpAdmin.declareBinding(priceBinding);
+    }
+}
---
+++ src/main/java/com/microsservico/estoquepreco/constants/RabbitMQConstants.java
@@ -0,0 +1,6 @@
+package com.microsservico.estoquepreco.constants;
+
+public class RabbitMQConstants {
+    public static final String STOCK_QUEUE = "STOCK";
+    public static final String PRICE_QUEUE = "PRICE";
+}
